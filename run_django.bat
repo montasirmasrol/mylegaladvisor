64878006badabbe18c6cf8daa09d5c0ea2b00943
Montasir Chowdhury Masrol <montasir.masrol@gmail.com>
2025-11-17 12:13:20 +0600

    Delete run_django.bat

--- run_django.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Activate the virtual environment and run Django development server
-cd /d "%~dp0"
-call C:/Users/Masrol/.virtualenvs/Django-qD6mFPwX/Scripts/activate.bat
-python manage.py runserver
-pause
